599d7a5a7f268107248773e983ea92f8ecc0bccf
Joaquim <dacostaj.68@gmail.com>
2019-07-01 13:03:39 +0200

    mise a jour initialscript

---
+++ www/initialscript.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE "AnnuaireToutou";
+
+USE "Annuaire toutou";
+
+CREATE USER 'adminToutou'@'localhost' IDENTIFIED BY 'Annu@ireT0ut0u';
+
+GRANT ALL PRIVILEGES ON AnnuaireToutou.* TO 'adminToutou'@'localhost';
+
+CREATE TABLE Maitres (id INT PRIMARY KEY AUTO_INCREMENT,
+ nom VARCHAR(200),
+  telephone VARCHAR(20)),
+
+CREATE TABLE Chiens (id INT PRIMARY KEY AUTO_INCREMENT,
+ nom VARCHAR(255),
+  age INT,
+   race VARCHAR(200),
+    id_maitre INT,
+     FOREIGN KEY(id_maitre) REFERENCES Maitres(id));
+
+
+
+
+
